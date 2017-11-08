2d825f68ddda38ba2a45b8f57085525e49a7fd07
Chrisna Aing <chrisna@chrisna.org>
2017-11-07 23:30:27 -0800

    Add all of the initial files!

---
+++ README.md
@@ -0,0 +1,10 @@
+# eslint-plugin-ccrazy88-react
+
+A personalized, shareable ESLint configuration.
+
+Depends upon [eslint-plugin-import] and [eslint-plugin-prettier] and extends
+[eslint-config-prettier].
+
+[eslint-plugin-import]: https://github.com/benmosher/eslint-plugin-import
+[eslint-plugin-prettier]: https://github.com/prettier/eslint-plugin-prettier
+[eslint-config-prettier]: https://github.com/prettier/eslint-config-prettier
---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+  extends: [
+    "ccrazy88",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "prettier/react"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ["html", "jsx-a11y", "react"],
+  rules: {
+    // JSX accessibility
+    "jsx-a11y/lang": "error",
+
+    // React
+    "react/boolean-prop-naming": "off",
+    "react/default-props-match-prop-types": "error",
+    "react/forbid-component-props": "error",
+    "react/forbid-elements": "error",
+    "react/forbid-prop-types": "error",
+    "react/forbid-foreign-prop-types": "off",
+    "react/no-array-index-key": "error",
+    "react/no-danger": "warn",
+    "react/no-did-mount-set-state": "off",
+    "react/no-did-update-set-state": "error",
+    "react/no-multi-comp": "error",
+    "react/no-redundant-should-component-update": "error",
+    "react/no-set-state": "off",
+    "react/no-typos": "error",
+    "react/no-unused-prop-types": "error",
+    "react/no-unused-state": "error",
+    "react/no-will-update-set-state": "error",
+    "react/prefer-es6-class": "error",
+    "react/prefer-stateless-function": "error",
+    "react/require-default-props": "error",
+    "react/require-optimization": "off",
+    "react/self-closing-comp": "error",
+    "react/sort-comp": "error",
+    "react/sort-prop-types": "off",
+    "react/style-prop-object": "error",
+    "react/void-dom-elements-no-children": "error",
+
+    // React -> JSX-specific rules
+    "react/jsx-boolean-value": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "react/jsx-handler-names": "error",
+    "react/jsx-no-bind": "off",
+    "react/jsx-no-literals": "off",
+    "react/jsx-curly-brace-presence": [
+      "error",
+      { props: "never", children: "never" }
+    ],
+    "react/jsx-pascal-case": "error",
+    "react/jsx-sort-props": "off"
+  },
+  settings: {
+    "html/report-bad-indent": "error",
+    "import/extensions": [".js", ".jsx"]
+  }
+};
