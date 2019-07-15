6b69eaf26868a52568266a9cfb216c40df5012d4
Chrisna Aing <chrisna@chrisna.org>
2019-07-14 19:04:22 -0700

    Update configuration and dependencies.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unused-modules
 module.exports = {
   extends: [
     "ccrazy88",
@@ -13,10 +14,6 @@ module.exports = {
   plugins: ["html", "jsx-a11y", "react"],
   rules: {
     // JSX accessibility
-    "jsx-a11y/label-has-for": [
-      "error",
-      { required: { some: ["nesting", "id"] } }
-    ],
     "jsx-a11y/lang": "error",
 
     // React
@@ -24,52 +21,95 @@ module.exports = {
     "react/button-has-type": "off",
     "react/default-props-match-prop-types": "error",
     "react/destructuring-assignment": "error",
+    "react/display-name": "off",
     "react/forbid-component-props": "error",
     "react/forbid-dom-props": "off",
     "react/forbid-elements": "error",
-    "react/forbid-prop-types": "error",
     "react/forbid-foreign-prop-types": "off",
+    "react/forbid-prop-types": "error",
     "react/no-access-state-in-setstate": "error",
     "react/no-array-index-key": "error",
+    "react/no-children-prop": "error",
     "react/no-danger": "warn",
+    "react/no-danger-with-children": "error",
+    "react/no-deprecated": "error",
     "react/no-did-mount-set-state": "off",
     "react/no-did-update-set-state": "error",
+    "react/no-direct-mutation-state": "off",
+    "react/no-find-dom-node": "error",
+    "react/no-is-mounted": "error",
     "react/no-multi-comp": "off",
     "react/no-redundant-should-component-update": "error",
+    "react/no-render-return-value": "error",
     "react/no-set-state": "off",
+    "react/no-string-refs": "error",
     "react/no-this-in-sfc": "error",
     "react/no-typos": "error",
+    "react/no-unescaped-entities": "error",
+    "react/no-unknown-property": "error",
+    "react/no-unsafe": "off",
     "react/no-unused-prop-types": "error",
     "react/no-unused-state": "error",
     "react/no-will-update-set-state": "error",
     "react/prefer-es6-class": "error",
+    "react/prefer-read-only-props": "off",
     "react/prefer-stateless-function": "error",
+    "react/prop-types": "error",
+    "react/react-in-jsx-scope": "error",
     "react/require-default-props": "error",
     "react/require-optimization": "off",
+    "react/require-render-return": "error",
     "react/self-closing-comp": "error",
     "react/sort-comp": "error",
     "react/sort-prop-types": "off",
+    "react/state-in-constructor": "off",
+    "react/static-property-placement": "off",
     "react/style-prop-object": "error",
     "react/void-dom-elements-no-children": "error",
 
     // React -> JSX-specific rules
     "react/jsx-boolean-value": "error",
     "react/jsx-child-element-spacing": "off",
-    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
-    "react/jsx-handler-names": "error",
-    "react/jsx-max-depth": "off",
-    "react/jsx-no-bind": "off",
-    "react/jsx-no-literals": "off",
+    "react/jsx-closing-bracket-location": "off",
+    "react/jsx-closing-tag-location": "off",
     "react/jsx-curly-brace-presence": [
       "error",
       { props: "never", children: "never" }
     ],
+    "react/jsx-curly-newline": "off",
+    "react/jsx-curly-spacing": "off",
+    "react/jsx-equals-spacing": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "react/jsx-first-prop-new-line": "off",
+    "react/jsx-fragments": "error",
+    "react/jsx-handler-names": "error",
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-key": "error",
+    "react/jsx-max-depth": "off",
+    "react/jsx-max-props-per-line": "off",
+    "react/jsx-no-bind": "off",
+    "react/jsx-no-comment-textnodes": "error",
+    "react/jsx-no-duplicate-props": "error",
+    "react/jsx-no-literals": "off",
+    "react/jsx-no-target-blank": "error",
+    "react/jsx-no-undef": "error",
+    "react/jsx-one-expression-per-line": "off",
     "react/jsx-pascal-case": "error",
+    "react/jsx-props-no-multi-spaces": "off",
+    "react/jsx-props-no-spreading": "error",
     "react/jsx-sort-default-props": "off",
-    "react/jsx-sort-props": "off"
+    "react/jsx-sort-props": "off",
+    "react/jsx-tag-spacing": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-wrap-multilines": "off"
   },
   settings: {
     "html/report-bad-indent": "error",
-    "import/extensions": [".js", ".jsx"]
+    "import/extensions": [".js", ".jsx"],
+    react: {
+      version: "detect"
+    }
   }
 };
